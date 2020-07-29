4f2927c685b49b6bf82763fcd92b1b99ccf35e41
Claes Wolff <claes-jan.wolff@hotmail.com>
2020-07-29 14:05:00 +0200

    Create get_more_tweets.py

---
+++ get_more_tweets.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+print(keras.backend.backend())
+
+consumer_key = "<your consumer key here>"
+consumer_secret = "<your consumer secret here>"
+access_key = "<your access key here>"
+access_secret = "<your access secret>"
